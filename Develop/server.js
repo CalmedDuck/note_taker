d0aafe116f643d031741105c145f0d1a197f8064
Brock Williams <b.williams54255@gmail.com>
2023-10-14 20:45:42 -0700

    Added middleware using api routes and .json. Added routes when a GET request is made to /notes and a wildcard route for index.html. Added app listening upon succesfull deployment of the server.

--- Develop/server.js
+++ Develop/server.js
@@ -8,3 +8,19 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);  
+app.use(express.static('public'));
+
+app.get('/notes', (req, res) => 
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.get('*', (req, res) => 
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.listen(PORT, () => 
+  console.log(`App listening at http://localhost:${PORT}`)
+);
